524f281c2200a19e54f1ab0b6d63faa05b5fa007
firasskarboul <fkarboul@gmail.com>
2024-05-20 21:40:36 +0200

    Task 1 and 2: Configurate 2 containers (postgres and pgAdmin4), generate User entity/repository and create register API (email/password)

--- migrations/Version20240520193021.php
+++ migrations/Version20240520193021.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240314102517 extends AbstractMigration
+final class Version20240520193021 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,8 +21,11 @@ final class Version20240314102517 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE utilisateur_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE book (id INT NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, author VARCHAR(255) NOT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, category VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN book.published_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE utilisateur (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON utilisateur (email)');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +33,8 @@ final class Version20240314102517 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE utilisateur_id_seq CASCADE');
         $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE utilisateur');
     }
 }
